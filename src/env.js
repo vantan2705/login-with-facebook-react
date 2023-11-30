ea5e2ae24bbfdd0d61112715172d56ec4792cbc3
vantan2705 <43321300+vantan2705@users.noreply.github.com>
2023-11-30 17:13:01 +0700

    First commit

---
+++ src/env.js
@@ -0,0 +1,5 @@
+// moved env variables into js file for stackblitz because it doesn't support dotenv (.env)
+export default {
+  REACT_APP_API_URL: 'http://localhost:3000',
+  REACT_APP_FACEBOOK_APP_ID: '752945683513834',
+};
